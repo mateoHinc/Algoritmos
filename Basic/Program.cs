78bc4dea3f7e4b6e6fab6f7e0fd72e2c26c5cc26
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-16 16:23:23 -0500

    Exercise Basic #2: Choose a option in the menu

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -4,17 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Ingrese el número: ");
-        int number = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Menú: \n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\nSeleccione una opción: ");
+        int option = Convert.ToInt32(Console.ReadLine());
 
-        if (number % 2 == 0)
+        switch (option)
         {
-            Console.Write("El número "+number+" es par");
+            case 1:
+                Console.Write("Seleccionaste sumar");
+                break;
+            case 2:
+                Console.Write("Seleccionaste restar");
+                break;
+            case 3:
+                Console.Write("Seleccionaste multiplicar");
+                break;
+            default :
+                Console.Write("Seleccionaste dividir");
+                break;
         }
-        else
-        {
-            Console.Write("El número "+number+" es impar");
-        }
+        
     }
 }
 
