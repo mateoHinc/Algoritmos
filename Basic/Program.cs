ee405fd2f35965b80878bb0b696033a73ec33537
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-16 16:06:21 -0500

    Initial commit

---
+++ Basic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el número: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.Write("El número es par");
+        }
+        else
+        {
+            Console.Write("El número es impar");
+        }
+    }
+}
+
