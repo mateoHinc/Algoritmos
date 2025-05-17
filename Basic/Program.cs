6dca263b0ec93fff93cae2efb05be286e0699669
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-16 21:08:07 -0500

    Exercise Basic #8: Calculate the greater of three numbers.

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -4,46 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Ingrese un año: ");
-        int year = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ingrese el primer número: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
 
-        if (year.ToString().Length == 4)
+        Console.Write("Ingrese el segundo número: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese el tercer número: ");
+        int number3 = Convert.ToInt32(Console.ReadLine());
+
+        if (number1 > number2 && number1 > number3)
         {
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-            {
-                Console.WriteLine($"El año {year} es un año bisiesto");
-            }
-            else
-            {
-                Console.WriteLine($"El año {year} no es un año bisiesto");
-            }
+            Console.WriteLine($"\nEl número {number1} es mayor que {number2} y {number3}");
+        }
+        else if (number2 > number1 && number2 > number3)
+        {
+            Console.WriteLine($"\nEl número {number2} es mayor que {number1} y {number3}");
+        }
+        else if (number3 > number1 && number3 > number1)
+        {
+            Console.WriteLine($"\nEl número {number3} es mayor que {number1} y {number2}");
+        }
+        else if (number1 == number2 && number1 == number3)
+        {
+            Console.WriteLine("\nTodos los números son iguales");
         }
         else
         {
-            Console.WriteLine($"El año {year} no tiene 4 digitos");
+            Console.WriteLine("\nHay dos números iguales y uno diferente");
         }
     }
 }
 
 /*
 
-if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-es la forma correcta de determinar si un año es bisiesto en 
-el calendario gregoriano. Vamos a descomponerla paso a paso.
-
-Un año es bisiesto si:
-
-1. Es divisible entre 4.
-    (year % 4 == 0)
-    ➤ Ejemplo: 2024 ÷ 4 = 506 → posible bisiesto
-
-2. Excepto si también es divisible entre 100.
-    (year % 100 != 0)
-    ➤ Ejemplo: 1900 es divisible por 4 y por 100 → ❌ no es bisiesto
-
-3. Pero si además es divisible entre 400, sí es bisiesto.
-    (year % 400 == 0)
-    ➤ Ejemplo: 2000 es divisible entre 100 y 400 → ✅ sí es bisiesto
 
 */
 
