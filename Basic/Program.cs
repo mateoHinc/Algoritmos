d8b8ea1f0cd459bf172d5343935d937746219dc9
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-16 21:18:14 -0500

    Exercise Basic #9: Check if a number is within the range 10-50.

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -4,34 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Ingrese el primer número: ");
-        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ingrese un número: ");
+        int number = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Ingrese el segundo número: ");
-        int number2 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Ingrese el tercer número: ");
-        int number3 = Convert.ToInt32(Console.ReadLine());
-
-        if (number1 > number2 && number1 > number3)
+        if (number >= 10 && number <= 50)
         {
-            Console.WriteLine($"\nEl número {number1} es mayor que {number2} y {number3}");
+            Console.WriteLine($"\nEl número {number} está entre el rango de 10 y 50 (Inclusive)");
         }
-        else if (number2 > number1 && number2 > number3)
+        else if (number < 10)
         {
-            Console.WriteLine($"\nEl número {number2} es mayor que {number1} y {number3}");
-        }
-        else if (number3 > number1 && number3 > number1)
-        {
-            Console.WriteLine($"\nEl número {number3} es mayor que {number1} y {number2}");
-        }
-        else if (number1 == number2 && number1 == number3)
-        {
-            Console.WriteLine("\nTodos los números son iguales");
+            Console.WriteLine($"\nEl número {number} es menor que 10");
         }
         else
         {
-            Console.WriteLine("\nHay dos números iguales y uno diferente");
+            Console.WriteLine($"\nEl número {number} es mayor que 50");
         }
     }
 }
